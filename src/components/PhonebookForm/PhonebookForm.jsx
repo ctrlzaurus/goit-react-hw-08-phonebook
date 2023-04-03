b4b1b8b7f5aa579a4ed091232c728b2bc3bdd522
ctrlzaurus <ichi.di@icloud.com>
2023-04-04 00:51:27 +0300

    new hw

--- src/components/PhonebookForm/PhonebookForm.jsx
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -22,6 +22,7 @@ class PhonebookForm extends Component {
         event.preventDefault();
         const contactId = {...this.state, id: shortid.generate()};
         this.props.addContacts(contactId);
+        event.target.reset();
     }
 
     static propTypes = {
@@ -31,7 +32,7 @@ class PhonebookForm extends Component {
 
     render() {
         return(
-            <form className={d.conteinerForm} onSubmit = {event => this.handleSubmit(event)} >
+            <form className={d.conteinerForm} onSubmit = {this.handleSubmit} >
                 <div>
                     <h3 className={d.title}>Name</h3>
                     <input
@@ -40,7 +41,7 @@ class PhonebookForm extends Component {
                         pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                         required
-                        value={this.state.name}
+                        // value={this.state.name}
                         onChange={this.handleChange}
                         className={d.input}
                     />
@@ -54,7 +55,7 @@ class PhonebookForm extends Component {
                         pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                         required
-                        value={this.state.number} 
+                        // value={this.state.number} 
                         onChange={this.handleChange}
                         className={d.input}
                     />        
